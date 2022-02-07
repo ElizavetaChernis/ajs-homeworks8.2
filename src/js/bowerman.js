eeddaae287bfb14cc1914a4d9801d9f937796a9a
ElizavetaChernis <elizaveta.chernysheva2@gmail.com>
2022-02-07 17:59:42 +0500

    first

--- src/js/bowerman.js
+++
@@ -1,9 +0,0 @@
-import Character from './character';
-export default class Bowerman extends Character {
-    constructor(name) {
-        super(name);
-        this.type = 'Bowman';
-        this.attack = 25;
-        this.deffence = 25;
-    }
-  }
