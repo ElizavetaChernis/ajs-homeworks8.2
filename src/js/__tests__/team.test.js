562e26d5be339246057c75b94d58634382a8c83b
ElizavetaChernis <elizaveta.chernysheva2@gmail.com>
2022-02-19 00:12:51 +0500

    first

--- src/js/__tests__/team.test.js
+++ src/js/__tests__/team.test.js
@@ -1,54 +1,14 @@
-import Team from '../Team';
-import Character from '../character';
+import ErrorRepository from '../ErrorRepository';
 
-test('should return add character', () => {
-  const character = new Character('John');
-  const team = new Team();
-  team.add(character);
+const errorRepository = new ErrorRepository();
+errorRepository.errors.set(400, 'Bad Request');
+errorRepository.errors.set(404, 'Not Found');
+errorRepository.errors.set(408, 'Request Timeout');
 
-  const expected = new Set();
-  expected.add(character);
-
-  expect(team.members).toEqual(expected);
+test('should return an error by code', () => {
+  expect(errorRepository.translate(404)).toBe('Not Found');
 });
 
-test('should return an error when re-adding a character', () => {
-  const character = new Character('John');
-  const team = new Team();
-  team.add(character);
-
-  expect(() => team.add(character)).toThrowError(new Error('Персонаж уже добавлен в команду'));
-});
-
-test('should add an arbitrary number of characters', () => {
-  const characterOne = new Character('John');
-  const characterTwo = new Character('Ivan');
-  const characterThree = new Character('Kate');
-
-  const team = new Team();
-  team.addAll(characterOne, characterTwo, characterThree);
-
-  const expected = new Set();
-  expected.add(characterOne);
-  expected.add(characterTwo);
-  expected.add(characterThree);
-
-  expect(team.members).toEqual(expected);
-});
-
-test('should return array', () => {
-  const characterOne = new Character('John');
-  const characterTwo = new Character('Ivan');
-  const characterThree = new Character('Kate');
-
-  const team = new Team();
-  team.addAll(characterOne, characterTwo, characterThree);
-
-  const expected = [
-    { health: 100, level: 1, name: 'John' },
-    { health: 100, level: 1, name: 'Ivan' },
-    { health: 100, level: 1, name: 'Kate' },
-  ];
-
-  expect(team.toArray()).toEqual(expected);
+test('should return an error if no such code exists', () => {
+  expect(errorRepository.translate(405)).toBe('Unknown error');
 });
